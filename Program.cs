daec56fb2af9c9e3791e20fd6173581fdec58db3
Hanna <varibrus.hanna@gmail.com>
2024-10-03 20:19:37 +0300

    Restore program

--- Program.cs
+++ Program.cs
@@ -8,8 +8,48 @@ namespace Project.Test
 {
     internal class Program
     {
+        private static void Terminal()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nSystem.");
+                Console.WriteLine("1. Get authors list.");
+                Console.WriteLine("2. Get author by identifier.");
+                Console.WriteLine("3. Exist.");
+                Console.WriteLine("Your choice: ");
+
+                var choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        GetAuthors();
+                        break;
+                    case 2:
+                        GetAuthorById();
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("\nEnter correct data!");
+                        break;
+                }
+            }
+        }
+
+        private static void GetAuthorById()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
